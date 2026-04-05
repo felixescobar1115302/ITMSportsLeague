cb2c9ffbc139ecb63ef66a81d0979a2dcf5f5dea
Félix Rolando Escobar Bolívar <felixescobar1115302@correo.itm.edu.co>
2026-04-04 21:44:06 -0500

    Implement sponsor and tournament sponsor phase 4

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class SponsorRepository : GenericRepository<Sponsor>, ISponsorRepository
+{
+    public SponsorRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Sponsor?> GetByNameAsync(string name)
+    {
+        return await _dbSet.FirstOrDefaultAsync(s => s.Name.ToLower() == name.ToLower());
+    }
+
+    public async Task<Sponsor?> GetByEmailAsync(string email)
+    {
+        return await _dbSet.FirstOrDefaultAsync(s => s.ContactEmail.ToLower() == email.ToLower());
+    }
+}
